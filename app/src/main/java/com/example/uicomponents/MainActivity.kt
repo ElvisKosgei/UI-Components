acda82fe3c35326aeee2e62d3e1b48ead59eca3d
ElvisKosgei <kosgeielvis06@mail.com>
2022-07-15 11:08:55 +0300

    UI Components up

---
+++ app/src/main/java/com/example/uicomponents/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.uicomponents
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import androidx.core.view.isVisible
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val progressCircular : ProgressBar = findViewById(R.id.progressBarCircular)
+        val progressHorizotal : ProgressBar = findViewById(R.id.progressBarHorizontal)
+        val buttonStart : Button = findViewById(R.id.buttonStart)
+        val buttonScroll : Button = findViewById(R.id.buttonScroll)
+
+        buttonStart.setOnClickListener {
+            if (progressCircular.isVisible) {
+                progressCircular.visibility = View.GONE
+                progressHorizotal.visibility = View.GONE
+
+            }else {
+                progressCircular.visibility = View.VISIBLE
+                progressHorizotal.visibility = View.VISIBLE
+            }
+        }
+
+        buttonScroll.setOnClickListener {
+            val intent = Intent(this, ScrollingActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
